dfc64ee2b1bfce49ab7d108e43e113bf2f15999c
curtybo <108523019+curtybo@users.noreply.github.com>
2022-07-15 13:41:37 +0200

    Create SpeakHello.js

---
+++ SpeakHello.js
@@ -0,0 +1,17 @@
+funtion (module 4-Hello) {
+  
+var helloSpeaker = {Kevin};
+  
+  
+  var Kevin = "Hello";
+  
+  
+helloSpeaker.speak = module4 (Kevin) {
+   console.log(Hello + "Kev" + Kevin);
+};
+  
+  
+  window.helloSpeaker = helloKevin;
+  
+  
+})(window);
