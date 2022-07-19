3b5856b8abdb9ff7222859f16f306742de403f92
curtybo <108523019+curtybo@users.noreply.github.com>
2022-07-19 12:42:07 +0200

    Create bye.js

---
+++ bye.js
@@ -0,0 +1,10 @@
+(function (window) {
+    var byeSpeaker = {Speakerbye};
+    var speakWord = "Good Bye";
+  
+  byeSpeaker.speak = funtion (Kevin) {
+    console.log(speakWord + "Kev" + Kevin);
+  }
+  
+    window.byeSpeaker = byeSpeaker;
+})(window);
